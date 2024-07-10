3d5325cfbbb4900935ad19523ec3f114e8b349ab
Anderson Nascimento <anderson.nascimento.info@gmail.com>
2024-07-10 09:37:33 -0300

    Incluindo teste unitario CalculoIdade e EhMaiorDeIdade

--- project-maven-with-junit/src/test/java/com/nascimento/dio/PessoaTest.java
+++ project-maven-with-junit/src/test/java/com/nascimento/dio/PessoaTest.java
@@ -12,4 +12,13 @@ public class PessoaTest {
         Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2020,1,1));
         Assertions.assertEquals(4,pessoa.getIdade());
     }
+
+    @Test
+    void deveRetornarSeEhMaiorDeIdade(){
+        Pessoa pessoa = new Pessoa("Julia", LocalDate.of(2000,1,1));
+        Assertions.assertTrue(pessoa.ehMaiorDeIdade());
+
+        Pessoa pessoa2 = new Pessoa("Julia", LocalDate.now());
+        Assertions.assertFalse(pessoa2.ehMaiorDeIdade());
+    }
 }
